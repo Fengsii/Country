c292d89a91be9ed989275081783d08724221320a
Efengsi Rahmanto Zalukhu <fengsifeng123@gmail.com>
2025-01-12 11:15:26 +0700

    Menggabunggak barnch

--- obj/Debug/net8.0/Country.AssemblyInfo.cs
+++ obj/Debug/net8.0/Country.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Country")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a843ba3287d1e112b59ed93aa37440e3e8fd7b87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc8884aee362705cd00b8f6ce280f546c56452c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Country")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Country")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
